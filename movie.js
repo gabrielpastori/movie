7db6e199d7e7580b6cff81363405ef483d66ade2
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-02-02 01:35:21 -0300

    Add styled page

--- movie.js
+++ movie.js
@@ -4,7 +4,7 @@ $("document").ready(function(){
     $("#add").on("click",function(e){
         e.preventDefault();
         var inputVal =parseInt($('#rating').val());
-        if(inputVal<0 || inputVal>10) alert("Rating value must be between 0 and 10")
+        if(inputVal<0 || inputVal>10 || Number.isNaN(inputVal)) alert("Rating value must be between 0 and 10")
         else {
             $("tbody").append('<tr><td>'+$('#title').val()+'</td><td>'+$('#rating').val()+'</td><td class="tdDelete"><button class="delete">delete</button></td></tr>')
             $('#title').val("");
